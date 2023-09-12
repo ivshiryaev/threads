35aa5c3bfbe97157152859f1b184539e856d4231
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-12 20:23:16 +0200

    style root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,11 @@ import '@/app/globals.css'
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 
+import Topbar from '@/components/shared/Topbar'
+import Bottombar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+
 export const metadata: Metadata = {
   title: 'Threads',
   description: 'Threads app',
@@ -16,7 +21,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          {children}
+          <Topbar/>
+          <main>
+            <LeftSidebar/>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            <RightSidebar/>
+          </main>
+          <Bottombar/>
         </body>
       </html>
     </ClerkProvider>
