a4ec9810070afc3c01b1e40860977a9d9bc12120
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-12 17:43:43 +0200

    wrap layout in <ClerkProvider>

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/globals.css'
 import type { Metadata } from 'next'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata: Metadata = {
   title: 'Threads',
@@ -12,10 +13,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='bg-dark-1'>
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className='bg-dark-1'>
+        <body>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
