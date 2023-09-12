48a82fa19ac44a4fae2b72ed7a86c57ae6717b60
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-12 11:41:04 +0200

    Installed all the necessary dependencies

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import '@/app/globals.css'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Threads',
+  description: 'Threads app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
