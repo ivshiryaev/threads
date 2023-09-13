a8324d2427b291e0efb598570dd8ed90156b269f
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-13 12:56:27 +0200

    style onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-
 function Page() {
 	return (
-		<div className='text-white'>
-			Onboarding
-		</div>
+		<main 
+			className='
+				flex flex-col justify-start
+				gap-8
+				px-10 py-20 
+				mx-auto 
+				max-w-3xl
+			'
+		>
+			<div className='flex flex-col gap-2'>
+				<h1 className='head-text'>Onboarding</h1>
+				<p className='
+						text-base-regular text-light-2
+					'
+				>
+					Complete your profile to use threads
+				</p>
+			</div>
+			<section className='bg-dark-2 p-10 rounded-lg'>
+				
+			</section>
+		</main>
 	)
 }
 
