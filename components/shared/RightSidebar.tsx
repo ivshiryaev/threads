8373131d8932c8d60b1dd96f10f733ea06767264
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-13 10:33:18 +0200

    add suggested communities to rightsidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -6,8 +6,27 @@ function RightSidebar(){
 					justify-start
 				'
 			>
-				<h3>Suggested communities</h3>
-			</div>			
+				<h3 className='
+						text-heading4-medium 
+						text-light-1
+					'
+				>
+					Suggested communities
+				</h3>
+			</div>
+			<div className='
+					flex flex-1 flex-col
+					justify-start
+				'
+			>
+				<h3 className='
+						text-heading4-medium 
+						text-light-1
+					'
+				>
+					Suggested users
+				</h3>
+			</div>		
 		</section>
 	)
 }
