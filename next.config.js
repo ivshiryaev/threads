d47264c687913927452c4b17c4acb39f9e3fdeeb
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-13 18:20:06 +0200

    update next.config.js to accept external images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+	experimental: {
+		serverActions: true,
+		serverComponentsExternalPackages: ['@acme/ui'],
+	},
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.clerk.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+}
