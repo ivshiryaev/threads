b732e33c03fea5cc11ba3199f14547f397b1353a
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-15 16:20:04 +0200

    create mongoDB connectToDB

---
+++ lib/mongoose.ts
@@ -0,0 +1,21 @@
+"use server"
+
+import mongoose from 'mongoose'
+
+let isConnected = false
+
+export async function connectToDB(){
+	// Set strict query mode for Mongoose to prevent unknown field queries.
+	mongoose.set("strictQuery", true);
+
+	if(isConnected) return console.log('Already connected to mongoDB!')
+	if(!process.env.MONGODB_URL) return console.log('MONGODB_URL isn\'t found')
+
+	try {
+		await mongoose.connect(process.env.MONGODB_URL)
+		isConnected = true
+		console.log('MongoDB connected')
+	} catch(e) {
+		console.error({message: e.message})
+	}
+}
