fbc1a120740ffaea2fef2149ec5fa0a9db333219
Ivan Shyriaiev <ivshiryaev1999@gmail.com>
2023-09-16 18:31:59 +0200

    Create basic threadCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
+import { fetchThreads } from '@/lib/actions/thread.actions'
+import { currentUser } from '@clerk/nextjs'
+import { ThreadCard } from '@/components/cards/ThreadCard'
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser()
+  
+  //TODO: HARDCODED
+  const result = await fetchThreads(1, 30)
+
   return (
-    <div className='text-white text-heading3-bold'>
-      Home
+    <div className='flex flex-col gap-6'>
+      <h1 className='head-text text-left'>
+        Home
+      </h1>
+      <section className='flex flex-col gap-6'>
+        {result.threads.length === 0 ? (
+            <p className='text-white italic'>No threads found</p>
+          ) : (
+            result.threads.map(post => (
+              <ThreadCard 
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post?.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.comments}
+              />
+            ))
+        )}
+      </section>
     </div>
   )
 }
---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,110 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+interface Props{
+	id : string,
+	currentUserId : string,
+	parentId : string | null,
+	content : string,
+	author : {
+		name: string,
+		image: string,
+		id: string,
+	},
+	community : {
+		id: string,
+		name: string,
+		image: string,
+	} | null,
+	createdAt : string,
+	comments : {
+		author: {
+			image: string,
+		}
+	}[],
+	isComment?: boolean,
+}
+
+function ThreadCard({
+	id,
+	currentUserId,
+	parentId,
+	content,
+	author,
+	community,
+	createdAt,
+	comments,
+} : Props) {
+	return (
+		<article 
+			className='
+				w-full
+				rounded-xl
+				p-6
+				flex gap-3
+				bg-dark-2
+				text-white
+			'
+		>
+			<div className='flex flex-col items-center gap-2'>
+				<Link href={`/profile/${author.id}`}>
+					<Image 
+						alt={author.username}
+						src={author.image}
+						width={44}
+						height={44}
+						className='rounded-full'
+					/>
+				</Link>
+				<div 
+					className='w-0.5 relative bg-neutral-800 rounded-full grow'
+				/>
+			</div>
+			<div 
+				className='
+					flex flex-col gap-1 flex-1
+				'
+			>
+				<Link 
+					className='w-fit font-semibold'
+					href={`/profile/${author.id}`}
+				>
+					{author.username}
+				</Link>
+				<p className='text-small-regular'>
+					{content}
+				</p>
+				<div className='flex gap-1 mt-2'>
+					<Image
+						alt='heart-icon'
+						src='/assets/heart-gray.svg'
+						width={24}
+						height={24}
+					/>
+					<Link href={`/thread/${id}`}>
+						<Image
+							alt='reply-icon'
+							src='/assets/reply.svg'
+							width={24}
+							height={24}
+						/>
+					</Link>
+					<Image
+						alt='repost-icon'
+						src='/assets/repost.svg'
+						width={24}
+						height={24}
+					/>
+					<Image
+						alt='share-icon'
+						src='/assets/share.svg'
+						width={24}
+						height={24}
+					/>
+				</div>
+			</div>
+		</article>
+	)
+}
+
+export { ThreadCard }
